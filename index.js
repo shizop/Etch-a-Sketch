0c6815d412612e57aba1a476fc33ec457cadebae
shizop <44642109+shizop@users.noreply.github.com>
2024-04-25 15:17:05 +0800

    work on random black reset

---
+++ index.js
@@ -0,0 +1,52 @@
+let color = "black";
+
+document.addEventListener("DOMContentLoaded", function () {
+  createContainer(16);
+  let resizeGridButton = document.querySelector("#resizeGrid");
+  resizeGridButton.addEventListener("click", () => {
+    let size = getSize();
+    createContainer(size);
+  });
+});
+
+function createContainer(size) {
+  let container = document.querySelector(".container");
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", colorDiv);
+    container.insertAdjacentElement("beforeend", div);
+  }
+}
+
+function getSize() {
+  let inputSize = prompt("Enter grid size (max 100): ");
+  inputSize = parseInt(inputSize);
+  if (Number.isInteger(inputSize) && inputSize > 0 && inputSize < 100) {
+    alert("You can play!");
+    return inputSize;
+  } else {
+    alert("Please enter a number between 0 and 100");
+  }
+}
+
+function colorDiv() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function reset() {
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
+}
